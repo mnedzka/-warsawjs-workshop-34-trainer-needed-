49b327f7f96bd36bdb8211c33fb5e4291098e599
Michal Nedzka <Michal.Nedzka@stepstone.com>
2019-07-28 11:16:13 +0200

    Setup server in node

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,26 @@ const FILE_EXTENSION_TO_CONTENT_TYPE: FileExtensionToContentTypeMap = {
   ico: 'image/x-icon',
   js: 'text/javascript',
 };
+
+const server = http.createServer((req, res) => {
+  try {
+    const url = req.url && req.url !== '/' ? req.url : '/index.html';
+    const urlParts = url.split('.')
+    const fileExtension = urlParts.slice(-1)[0] as StaticFileExtension;
+    const contentType = FILE_EXTENSION_TO_CONTENT_TYPE[fileExtension];
+
+    res.writeHead(200, { 'Content-Type': contentType });
+
+    const file = readFileSync(`${process.cwd()}/public${url}`);
+
+    res.end(file);
+
+  } catch (e) {
+    return res.end(e.toString())
+  }
+
+  res.end();
+})
+
+
+server.listen(PORT, () => console.log(`Hello I'm up on port ${PORT}`))
